db33b81ac519ed5e1c55f36e8197beec4ac37a41
winston <hey@winston.sh>
2023-10-12 01:14:45 +0200

    refactor: alpinejs html injection

--- plugins/config/after/queries/html/injections.scm
+++ plugins/config/after/queries/html/injections.scm
@@ -1,48 +1,37 @@
 ;; extends
 (attribute
-  (attribute_name) @attribute_name
-    (#any-of? @attribute_name
-      "x-data"
-      "x-effect"
-      "x-for"
-      "x-html"
-      "x-id"
-      "x-if"
-      "x-init"
-      "x-model"
-      "x-modelable"
-      "x-ref"
-      "x-show"
-      "x-teleport"
-      "x-text"
-      ":class"
-      ":id"
-      ":placeholder"
-      ":selected"
-      ":style"
+  (
+    [
+      ((attribute_name) @alpine.simple
+        (#any-of? @alpine.simple
+          "x-data"
+          "x-effect"
+          "x-for"
+          "x-html"
+          "x-id"
+          "x-if"
+          "x-init"
+          "x-model"
+          "x-modelable"
+          "x-ref"
+          "x-show"
+          "x-teleport"
+          "x-text"
+          ":class"
+          ":id"
+          ":placeholder"
+          ":selected"
+          ":style"
+        ))
+      ((attribute_name) @alpine.at-shortcut
+        (#match? @alpine.at-shortcut "^\\@.*$"))
+      ((attribute_name) @alpine.x-on
+        (#match? @alpine.x-on "^x-on:.+$"))
+      ((attribute_name) @alpine.x-bind
+        (#match? @alpine.x-bind "^x-bind:.+$"))
+    ]
+    (quoted_attribute_value
+      (attribute_value) @injection.content (#set! injection.language "javascript")
     )
-  (quoted_attribute_value
-    (attribute_value) @injection.content (#set! injection.language "javascript")
-  )
-)
-(attribute
-  (attribute_name) @attribute_name
-    (#match? @attribute_name "\\@.*$")
-  (quoted_attribute_value
-    (attribute_value) @injection.content (#set! injection.language "javascript")
-  )
-)
-(attribute
-  (attribute_name) @attribute_name
-    (#match? @attribute_name "^x-on:.+$")
-  (quoted_attribute_value
-    (attribute_value) @injection.content (#set! injection.language "javascript")
-  )
-)
-(attribute
-  (attribute_name) @attribute_name
-    (#match? @attribute_name "^x-bind.+$")
-  (quoted_attribute_value
-    (attribute_value) @injection.content (#set! injection.language "javascript")
   )
 )
